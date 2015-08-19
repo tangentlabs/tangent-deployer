d4db6c37fa9d8cd30ec7f046569b55bc5d2dd068
Chris McKinnel <chrismckinnel@gmail.com>
2015-08-19 12:18:03 +0100

    Improve file uploading

--- src/tangentdeployer/aws/s3.py
+++ src/tangentdeployer/aws/s3.py
@@ -8,13 +8,14 @@ from fabconfig import env
 def push_config_to_s3():
     utils.status('Pushing %(environment)s config to S3' % env)
     bucket = env.connections.s3.get_bucket(env.s3_bootstrap_bucket)
-    for (source_dir, directory, filename) in os.walk(env.bootstrap_folder):
-        full_key_name = os.path.join(env.environment, 'bootstrap', filename)
-        key = bucket.new_key(full_key_name)
-        full_path = os.path.join(source_dir, directory, filename)
-        contents = get_bootstrap_file(full_path)
-        key.set_contents_from_string(contents)
-        key.set_acl('authenticated-read')
+    for (dirpath, dirname, filenames) in os.walk(env.bootstrap_folder):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            key_name = os.path.join(env.environment, filepath)
+            key = bucket.new_key(key_name)
+            contents = get_bootstrap_file(filepath)
+            key.set_contents_from_string(contents)
+            key.set_acl('authenticated-read')
     utils.success('Finished pushing deploy script to S3')
 
 
